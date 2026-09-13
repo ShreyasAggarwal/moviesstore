d757111ebaf6d89deb5f412e346aef7488fe5d9a
Shreyas Aggarwal <shreyasa79@gmail.com>
2026-09-13 18:11:29 -0400

    adding 21st user story functionality

--- movies/models.py
+++ movies/models.py
@@ -19,5 +19,7 @@ class Review(models.Model):
         on_delete=models.CASCADE)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
+    reported = models.BooleanField(default=False)
+    reporting_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='reported')
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
